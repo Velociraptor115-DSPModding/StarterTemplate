ca3cd4880f2844db44e8dc1d50eae398ce139af1
Velociraptor115 <Velociraptor115@users.noreply.github.com>
2022-03-18 15:46:24 +0530

    Initial Commit

---
+++ StarterTemplate/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace DysonSphereProgram.Modding.StarterTemplate
+{
+  [BepInPlugin(GUID, NAME, VERSION)]
+  [BepInProcess("DSPGAME.exe")]
+  public class Plugin : BaseUnityPlugin
+  {
+    public const string GUID = "your.guid.here.StarterTemplate";
+    public const string NAME = "StarterTemplate";
+    public const string VERSION = "1.0.0";
+
+    private Harmony _harmony;
+    internal static ManualLogSource Log;
+
+    private void Awake()
+    {
+      Plugin.Log = Logger;
+      _harmony = new Harmony(GUID);
+      Logger.LogInfo("StarterTemplate Awake() called");
+    }
+
+    private void OnDestroy()
+    {
+      Logger.LogInfo("StarterTemplate OnDestroy() called");
+      _harmony?.UnpatchSelf();
+      Plugin.Log = null;
+    }
+  }
+}
